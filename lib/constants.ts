bd439b95f43f1aa62332a133388dad3749472916
CrazyDave0522 <davema0522@gmail.com>
2025-11-03 20:18:00 +0800

    feat: use online placeholder services for default images
    - Replace local image paths with Unsplash API for dish images
    - Use Placeholder.com for tag icons
    - No need to maintain local placeholder files

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,9 +4,9 @@ export const CONSTANTS = {
   // URLs
   ORDER_URL: 'https://www.ubereats.com/au/store/hotlob-elizabeth-street/NYteIBrURkOJEvArP0rBTA/5e126e11-ff14-4486-88e8-f50878f1d325?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkhvc3RlbCUyMEclMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjI1MTNkNDQ5MC04YjFlLWJkN2UtODlkNS03MGI3ODJmZTAyYTMlMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIydWJlcl9wbGFjZXMlMjIlMkMlMjJsYXRpdHVkZSUyMiUzQS0zMS45NTAwOTIlMkMlMjJsb25naXR1ZGUlMjIlM0ExMTUuODY0NjExJTdE',
   
-  // Default values
-  DEFAULT_DISH_IMAGE: '/images/default-dish.jpg',
-  DEFAULT_TAG_ICON: '/images/default-icon.png',
+  // Default values (using online placeholder services)
+  DEFAULT_DISH_IMAGE: 'https://source.unsplash.com/800x600/?lobster,seafood',
+  DEFAULT_TAG_ICON: 'https://via.placeholder.com/80x80/f0f0f0/666666?text=Tag',
   
   // Revalidation
   REVALIDATE_TIME: 60, // seconds
