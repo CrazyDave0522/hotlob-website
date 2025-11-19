3c594fe1a32ae49845922abc444218a2a61765bf
CrazyDave0522 <davema0522@gmail.com>
2025-11-19 16:49:39 +0800

    adjust more button

--- app/components/SeeOurFoodSection.tsx
+++ app/components/SeeOurFoodSection.tsx
@@ -63,7 +63,7 @@ export default function SeeOurFoodSection({ dishes }: SeeOurFoodSectionProps) {
         onClick={() => router.push("/see-our-food")}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
-        className="flex flex-col items-center mt-10 transition-colors duration-200"
+        className="flex flex-col items-center mt-12 transition-colors duration-200"
         style={{ gap: 10 }}
       >
         <div
