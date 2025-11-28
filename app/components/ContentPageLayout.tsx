1fd166715b3020175270c4d08de81f7615b91f8c
CrazyDave0522 <davema0522@gmail.com>
2025-11-28 22:57:50 +0800

    fix import lint

--- app/components/ContentPageLayout.tsx
+++ app/components/ContentPageLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactNode } from "react";
 
 type ContentPageLayoutProps = {
   title: string;
