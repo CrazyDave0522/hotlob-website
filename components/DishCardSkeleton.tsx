fbb417fa7b885589349db2631ea487fb81df7b84
CrazyDave0522 <davema0522@gmail.com>
2026-02-18 13:09:34 +0800

    Adjust dish card ratios and skeleton

--- components/DishCardSkeleton.tsx
+++ components/DishCardSkeleton.tsx
@@ -2,14 +2,12 @@ export function DishCardSkeleton() {
   return (
     <div className="DishCard DishCard--skeleton">
       <div className="DishCard-wrapper">
-        <div className="DishCard-image DishCard-image--skeleton" />
         <div className="DishCard-content">
           <div className="DishCard-title DishCard-title--skeleton" />
           <div className="DishCard-icons DishCard-icons--skeleton" />
           <div className="DishCard-tag DishCard-tag--skeleton" />
           <div className="DishCard-description DishCard-description--skeleton" />
         </div>
-        <div className="DishCard-button DishCard-button--skeleton" />
       </div>
     </div>
   );
